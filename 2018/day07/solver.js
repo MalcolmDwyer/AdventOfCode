5a42a6ae8e06c1d447f76a0d6e345df6188e3d8e
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2018-12-07 00:21:17 -0600

    2018: day07 part 2

--- 2018/day07/solver.js
+++ 2018/day07/solver.js
@@ -24,11 +24,6 @@ const getData = lines => lines.map((line, ix) => {
   return line
 })
 
-const abc = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
-// const base = 60
-const base = 0
-const abcT = letter => abc.indexOf(letter) + base + 1
-
 const solver1 = lines => {
   let data = getData(lines)
   console.log(data)
@@ -61,16 +56,37 @@ const solver1 = lines => {
   console.log('1 ***: ', doneSteps.join(''))
 }
 
+const workers = 5
+const base = 60
+// const workers = 2
+// const base = 0
+
+const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const abcT = letter => abc.indexOf(letter) + base + 1
+
 const solver2 = lines => {
   let data = getData(lines)
-  console.log(data)
+  // console.log(data)
   let time = 0
 
   let allSteps = new Set([...data.map(d => d[0]), ...data.map(d => d[1])])
   let doneSteps = []
+  let pendingSteps = new Set([])
 
-  let iter = 10000
-  while (allSteps.size) {
+  let timeSteps = {}
+
+  let iter = 1000000
+  while ((allSteps.size || pendingSteps.size) && iter) {
+    console.log('-------------------', time)
+    let done = timeSteps[time]
+    if (done && done.length) {
+      console.log(`finished ${done.join(', ')} at ${time}`)
+      done.sort().forEach(d => {
+        doneSteps.push(d)
+        pendingSteps.delete(d)
+      })
+    }
+
     // console.log('------')
     let readySteps = Array.from(allSteps).filter(step => {
       if (!data.some(d => d[1] === step)) {
@@ -81,17 +97,28 @@ const solver2 = lines => {
       }
       return false
     })
-    console.log('   readySteps', readySteps)
-    const done = Array.from(readySteps).sort()[0]
+    // console.log('   readySteps', readySteps)
+    // const done = Array.from(readySteps).sort()[0]
+    readySteps.slice(0, workers).forEach(readyStep => {
+      // console.log('             ** readyStep', readyStep, abcT(readyStep))
+      console.log(`${time}: ${readyStep} will be ready at +${abcT(readyStep)} => ${time + abcT(readyStep)}`)
+      // timeSteps[time + abcT(readyStep)] = readyStep
+      if (!timeSteps[time + abcT(readyStep)]) {
+        timeSteps[time + abcT(readyStep)] = []
+      }
+      timeSteps[time + abcT(readyStep)].push(readyStep)
+      allSteps.delete(readyStep)
+      pendingSteps.add(readyStep)
+    })
 
-    doneSteps.push(done)
-    allSteps.delete(done)
-    console.log('   remaining ', allSteps)
-    console.log('   :: done', doneSteps)
+    // console.log('   remaining ', allSteps)
+    // console.log('   :: done', doneSteps)
+    // console.log('   timeSteps', timeSteps)
     iter--
+    time++
   }
 
-
+// < 865
 
 
   console.log('2 ***: ', time)
