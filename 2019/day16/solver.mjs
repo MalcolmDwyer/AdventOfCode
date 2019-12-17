64ae6696665ed0a1e8268d1d4409dafe232baf63
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2019-12-16 21:01:57 -0600

    2019:16p2

--- 2019/day16/solver.mjs
+++ 2019/day16/solver.mjs
@@ -23,11 +23,10 @@ async function solver() {
 
 solver();
 
-const run = (data, part2) => {
+const run = (data, part2, offset) => {
   // console.log('day16');
   let phaseCount = 100;
   let blowup = 10000;
-  let sumStr;
 
   let vals = [...data];
   if (part2) {
@@ -37,65 +36,91 @@ const run = (data, part2) => {
     }
   }
 
+  let sliceOffset = 0;
+
+  if (part2) {
+    sliceOffset = parseInt(data.slice(0, 7).join(''));
+    // sliceOffset = 79990; // 000; /////////////// ************************************************ REMOVE
+    console.log('sliceOffset:', sliceOffset);
+  }
+  
+  // console.log('start', vals.join(''));
+  vals = vals.slice(sliceOffset);
+  // console.log('start', vals.join(''));
+
   let prevT = new Date();
   for (let t = 0; t < phaseCount; t++) {
     let now = new Date();
     console.log('PHASE', t, vals.length, now.valueOf() - prevT.valueOf());
     prevT = now;
     let newVals = [];
-    for (let s = 0; s < vals.length; s++) {
+
+    for (let s = vals.length - 1; s >= 0; s--) {
+      // let p = new Pattern(s + 1);
+      // let n;
+
+      let sum = (newVals.length ? newVals[0] : 0) + vals[s];
+
+      newVals.unshift(sum%10);
+      // if (!(s%1000)) {
+      //   console.log(`${s}    ${newVals.length}`);
+      // }
+    }
+
+    /*
+    for (let s = sliceOffset; s < vals.length + sliceOffset; s++) {
       let p = new Pattern(s + 1);
-      // let slices = vals.map(v => {
-      //   return v * p.next();
-      // });
-      // const sum = slices.reduce((acc, v) => acc + v, 0);
-      // const sum = vals.reduce((acc, v) => acc + (v * p.next()), 0);
-
-      // const sum = vals.reduce((acc, v) => {
-      //   let n = p.next();
-      //   if (!n) {
-      //     return acc;
-      //   }
-      //   return acc + (n * v);
-      // }, 0);
-
       let sum = 0;
       let n;
-      for (let v = 0; v < vals.length; v++) {
-        n = p.next();
-        if (!n) continue;
-        if (n == 1) {
-          sum = sum + vals[v];
+
+      n = p.skip(sliceOffset);
+
+      let innerDone = false;
+      let vRemain = vals.length;
+      let vp = 0;
+      while (!innerDone) {
+        const {factor, repeat} = p.nextSequence();
+        let count = Math.min(vRemain, repeat);
+        if (factor) {
+          let innerSum = vals.slice(vp, vp + count).reduce((acc, d) => acc + d);
+          if (factor === -1) {
+            sum -= innerSum;
+          }
+          else {
+            sum += innerSum;
+          }
         }
-        else {
-          sum = sum - vals[v];
+        vRemain -= count;
+        vp += count;
+        if (!vRemain) {
+          innerDone = true;
         }
       }
+      
+
+      // console.log('sum', sum);
 
       // sumStr = sum%10;
       // newVals.push(Math.abs(sum%10));
       // sumStr = `${sum}`;
       sumStr = '' + sum;
       newVals.push(parseInt(sumStr[sumStr.length - 1]));
-      // console.log(`sum ${sum} ==> [${sumStr[sumStr.length - 1]}] ==> [${parseInt(sumStr[sumStr.length - 1])}]`);
-      
-      // newVals.push(parseInt(sumStr.slice(-1)));
       // console.groupEnd();
     }
+    */
 
     // console.log('newVals', newVals.join(''));
     // vals = [...newVals];
     vals = newVals;
     
     // console.groupEnd();
+    console.log(t, vals.slice(0, 8).join(''));
+    // console.log(t, vals.slice(sliceOffset, sliceOffset + 8).join('') vals.slice(sliceOffset, sliceOffset + 8).join(''));
+    // console.log(t, 'slice', vals.slice(sliceOffset, sliceOffset + 8).join(''));
+
   }
 
-  let offset = 0;
-
-  if (part2) {
-    offset = parseInt(data.slice(0, 7).join(''))
-  }
-  console.log(vals.slice(offset, 8).join(''));
+  console.log('D:', vals.slice(0, 8).join(''));
 };
 
 
@@ -140,12 +165,32 @@ class Pattern {
     if (this.repeatCount === 0) {
       this.pointer = (this.pointer + 1) % 4;
       this.repeatCount = this.repeat;
-    }
-    
+    } 
 
     return retval;
   }
-  
+
+  skip(n) {
+    for (let t = 0; t < n; t++) {
+      this.repeatCount--;
+
+      if (this.repeatCount === 0) {
+        this.pointer = (this.pointer + 1) % 4;
+        this.repeatCount = this.repeat;
+      } 
+    }
+  }
+
+  nextSequence() {
+    let ret = {
+      factor: this.pattern[this.pointer],
+      repeat: this.repeatCount,
+    };
+    this.repeatCount = this.repeat;
+    this.pointer = (this.pointer + 1) % 4;
+
+    return ret;
+  }
 }
 
 const test = () => {
@@ -158,4 +203,13 @@ const test = () => {
 
   p = new Pattern(3);
   console.log(p.next(), p.next(), p.next(), p.next(), p.next(), p.next(), p.next(), p.next());
+
+  p = new Pattern(3);
+  p.skip(4);
+  console.log(p.next(), p.next(), p.next(), p.next());
+
+
+  console.log('sequence test');
+  p = new Pattern(3);
+  console.log(p.nextSequence(),p.nextSequence(),p.nextSequence(),p.nextSequence(),p.nextSequence());
 }
