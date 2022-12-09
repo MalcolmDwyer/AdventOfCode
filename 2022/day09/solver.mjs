c400bdc8eb2150487391b87c4dd515df5f0b3372
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2022-12-09 01:45:03 -0600

    2022d09 - more cleanup - GO TO BED!!!

--- 2022/day09/solver.mjs
+++ 2022/day09/solver.mjs
@@ -1,32 +1,20 @@
 import { lineReader } from '../../common.mjs';
 
 const parseLine = line => {
-  const distance = parseInt(line.slice(2));
+
   if (line[0] === 'R') {
-    return {
-      dx: 1,
-      dy: 0,
-    }
+    return { dx: 1, dy: 0 }
   }
   if (line[0] === 'L') {
-    return {
-      dx: -1,
-      dy: 0,
-    }
+    return { dx: -1, dy: 0 }
   }
   if (line[0] === 'U') {
-    return {
-      dx: 0,
-      dy: 1,
-    }
+    return { dx: 0, dy: 1 }
   }
-  return {
-    dx: 0,
-    dy: -1,
-  }
+  return { dx: 0, dy: -1 }
 };
 
-const solver2 = async (length = 10) => {
+const solver = async (length = 10) => {
   let lines = await lineReader('input.txt');
 
   let rope = [];
@@ -40,7 +28,6 @@ const solver2 = async (length = 10) => {
     let {dx, dy} = parseLine(line);
     let n = parseInt(line.slice(2));
     while (n) {
-
       rope[0].x += dx;
       rope[0].y += dy;
 
@@ -64,8 +51,8 @@ const solver2 = async (length = 10) => {
   console.log(visited.size);
 };
 
-await solver2(2);
-await solver2(10);
+await solver(2);
+await solver(10);
 
 
 
