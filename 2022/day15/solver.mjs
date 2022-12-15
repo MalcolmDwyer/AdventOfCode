25645655d9e9654f939eb1799ac8dbfd22c791a6
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2022-12-15 03:00:39 -0600

    2022d15

--- 2022/day15/solver.mjs
+++ 2022/day15/solver.mjs
@@ -1,14 +1,20 @@
-import { lineReader, gridsMinMaxFromCoords, prepGrid, printGrid2 } from '../../common.mjs';
+import { lineReader } from '../../common.mjs';
 
 const parseLine = (line) => {
   const parts = /Sensor at x=(-?\d*), y=(-?\d*): closest beacon is at x=(-?\d*), y=(-?\d*)/.exec(line);
+  const sx = parseInt(parts[1]);
+  const sy = parseInt(parts[2]);
+  const bx = parseInt(parts[3]);
+  const by = parseInt(parts[4]);
   return ([{
-    x: parseInt(parts[1]),
-    y: parseInt(parts[2]),
+    x: sx,
+    y: sy,
   }, {
-    x: parseInt(parts[3]),
-    y: parseInt(parts[4]),
-  }]);
+    x: bx,
+    y: by,
+  },
+  ((Math.abs(sx - bx) + Math.abs(sy - by))),
+]);
 };
 
 
@@ -16,23 +22,14 @@ const solver = async (file, checkRow) => {
   let sensorBeacons = await lineReader(file, parseLine);
   let clean = new Set([]);
 
-  sensorBeacons.forEach(([sensor, beacon]) => {
-    // console.group(sensor, beacon);
-    const dx = Math.abs(sensor.x - beacon.x);
-    const dy = Math.abs(sensor.y - beacon.y);
-    const md = dx + dy;
-    // console.log(dx, dy, ' => ', md);
-
+  sensorBeacons.forEach(([sensor, _, md]) => {
     const rowDiff = Math.abs(checkRow - sensor.y);
-    // console.log('rowDiff', rowDiff);
     if (rowDiff < md) {
       const colDiff = Math.abs(md - rowDiff);
       for(let x = sensor.x - colDiff; x <= sensor.x + colDiff; x++) {
-        // console.log('  ', x);
         clean.add(x);
       }
     }
-    // console.groupEnd();
   });
 
   sensorBeacons.forEach(([_, beacon]) => {
@@ -44,57 +41,26 @@ const solver = async (file, checkRow) => {
   console.log('p1', clean.size);
 }
 
+
+
 const solver2 = async (file, checkRange) => {
   let sensorBeacons = await lineReader(file, parseLine);
-
-  for (let y = 0; y <= checkRange; y++) {
-    if (!(y % 10)) { console.log(y);}
-    const checkRow = y;
-    let clean = new Set([]);
-
-    sensorBeacons.forEach(([sensor, beacon]) => {
-      // console.group(sensor, beacon);
-      const dx = Math.abs(sensor.x - beacon.x);
-      const dy = Math.abs(sensor.y - beacon.y);
-      const md = dx + dy;
-      // console.log(dx, dy, ' => ', md);
-  
-      const rowDiff = Math.abs(checkRow - sensor.y);
-      // console.log('rowDiff', rowDiff);
-      if (rowDiff < md) {
-        const colDiff = Math.abs(md - rowDiff);
-        let minX = Math.max(0, sensor.x - colDiff);
-        let maxX = Math.min(checkRange, sensor.x + colDiff);
-        for (let x = minX; x <= maxX; x++) {
-          // console.log('  ', x);
-          clean.add(x);
-        }
+  const boundaries = new Set([]);
+  sensorBeacons.forEach(([sensor, _, md]) => {
+    // NE
+    for (let x = sensor.x, y = sensor.y - md - 1; x < sensor.x + md + 1; x++, y++) {
+      if (x < 0 || x > checkRange || y < 0 || y > checkRange) {
+        continue;
       }
-      // console.groupEnd();
-      
-    });
-    // console.log(checkRow, 'clean:', clean.size);
-    if (clean.size === checkRange) { // only one that is not checkRange+1
-      console.log('y:', checkRow);
-
-      for (let x = 0; x <= checkRange; x++) {
-        if (!clean.has(x)) {
-          console.log('x', x);
-          const freq = (x * 4000000) + checkRow;
-          console.log('p2', freq);
-          break;
-        }
+      if (!sensorBeacons.some(([s2, _, md2]) => (
+        Math.abs(s2.x - x) + Math.abs(s2.y - y) <= md2
+      ))) {
+        console.log('p2', x * 4000000 + y);
       }
-
-      break;
     }
-  }
+    // SE, SW, NW ? ... no need apparently
+  });
+};
 
-  console.log('p2');
-}
-
-
-// await solver('test.txt', 10);
-// await solver('input.txt', 2000000);
-// await solver2('test.txt', 20);
+await solver('input.txt', 2000000);
 await solver2('input.txt', 4000000);
